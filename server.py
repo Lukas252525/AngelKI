334d1b5ed8c115e831e2f1774e1aea436783a959
Lukas252525 <lukas@mueller-hunsrueck.de>
2026-08-04 19:44:47 +0200

    serve frontend trough FastAPI

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 
 import main
 from main import angelki
 
+
 print("SERVER IMPORT:", main.__file__)
+
 
 
 app = FastAPI(
@@ -12,6 +16,25 @@ app = FastAPI(
     description="KI Angelanalyse",
     version="1.0"
 )
+
+
+
+# ----------------------------------
+# Frontend bereitstellen
+# ----------------------------------
+
+app.mount(
+    "/frontend",
+    StaticFiles(directory="frontend"),
+    name="frontend"
+)
+
+
+
+# ----------------------------------
+# CORS
+# ----------------------------------
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -19,14 +42,37 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+
+# ----------------------------------
+# AngelKI API
+# ----------------------------------
+
 @app.get("/angelki")
 def analyse():
 
     try:
+
         daten = angelki()
+
         return daten
 
+
     except Exception as e:
+
         return {
             "fehler": str(e)
         }
+
+
+
+# ----------------------------------
+# Startseite
+# ----------------------------------
+
+@app.get("/")
+def startseite():
+
+    return FileResponse(
+        "frontend/index.html"
+    )
